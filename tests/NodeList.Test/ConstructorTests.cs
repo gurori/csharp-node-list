83fb4b0ff3f3d4a16658f48b14172089ab9812f3
gurori <nazarovslava124@gmail.com>
2026-01-21 13:44:37 +0300

    test: constructor preserves collection order

--- tests/NodeList.Test/ConstructorTests.cs
+++ tests/NodeList.Test/ConstructorTests.cs
@@ -14,7 +14,23 @@ public sealed class ConstructorTests
     public void Constructor_WithSingleElement_CountIsOne()
     {
         NodeList<int> list = [52];
-    
+
         Assert.Single(list);
+    }
+
+    [Fact]
+    public void Constructor_WithCollection_PreservesOrder()
+    {
+        int[] source = [1, 2, 3, 4, 5];
+
+        var list = new NodeList<int>(source);
+
+        Assert.Equal(source.Length, list.Count);
+
+        int index = 0;
+        foreach (var item in list)
+        {
+            Assert.Equal(item, source[index++]);
+        }
     }
 }
