e3046f9180b38f356e276b05da3cc9c60865e78f
gurori <nazarovslava124@gmail.com>
2026-08-02 17:49:15 +0300

    test: add RemoveAt unit tests

---
+++ tests/NodeList.Test/RemoveAtTests.cs
@@ -0,0 +1,189 @@
+namespace NodeList.Test;
+
+public sealed class RemoveAtTests
+{
+    [Fact]
+    public void RemoveAt_EmptyList_ThrowsArgumentOutOfRangeException()
+    {
+        NodeList<int> list = [];
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(0));
+    }
+
+    [Fact]
+    public void RemoveAt_SingleElement_ListBecomesEmpty()
+    {
+        NodeList<int> list = [42];
+
+        list.RemoveAt(0);
+
+        Assert.Empty(list);
+        Assert.Equal(0, list.Count);
+    }
+
+    [Fact]
+    public void RemoveAt_FirstElement_RemovesElement()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        list.RemoveAt(0);
+
+        Assert.Equal(2, list.Count);
+
+        int[] expected = [2, 3];
+        int index = 0;
+
+        foreach (var item in list)
+        {
+            Assert.Equal(expected[index++], item);
+        }
+    }
+
+    [Fact]
+    public void RemoveAt_MiddleElement_RemovesElement()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        list.RemoveAt(1);
+
+        Assert.Equal(2, list.Count);
+
+        int[] expected = [1, 3];
+        int index = 0;
+
+        foreach (var item in list)
+        {
+            Assert.Equal(expected[index++], item);
+        }
+    }
+
+    [Fact]
+    public void RemoveAt_LastElement_RemovesElement()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        list.RemoveAt(2);
+
+        Assert.Equal(2, list.Count);
+
+        int[] expected = [1, 2];
+        int index = 0;
+
+        foreach (var item in list)
+        {
+            Assert.Equal(expected[index++], item);
+        }
+    }
+
+    [Fact]
+    public void RemoveAt_NegativeIndex_ThrowsArgumentOutOfRangeException()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(-1));
+    }
+
+    [Fact]
+    public void RemoveAt_IndexEqualToCount_ThrowsArgumentOutOfRangeException()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(list.Count));
+    }
+
+    [Fact]
+    public void RemoveAt_IndexGreaterThanCount_ThrowsArgumentOutOfRangeException()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(10));
+    }
+
+    [Fact]
+    public void RemoveAt_RemoveAllElements_ListBecomesEmpty()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        list.RemoveAt(2);
+        list.RemoveAt(1);
+        list.RemoveAt(0);
+
+        Assert.Empty(list);
+        Assert.Equal(0, list.Count);
+    }
+
+    [Fact]
+    public void RemoveAt_AfterClear_ThrowsArgumentOutOfRangeException()
+    {
+        NodeList<int> list = [1, 2, 3];
+
+        list.Clear();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(0));
+    }
+
+    [Fact]
+    public void RemoveAt_RemoveFirstTwice_RemovesCorrectElements()
+    {
+        NodeList<int> list = [1, 2, 3, 4];
+
+        list.RemoveAt(0);
+        list.RemoveAt(0);
+
+        int[] expected = [3, 4];
+        int index = 0;
+
+        foreach (var item in list)
+        {
+            Assert.Equal(expected[index++], item);
+        }
+
+        Assert.Equal(2, list.Count);
+    }
+
+    [Fact]
+    public void RemoveAt_RemoveMiddleRepeatedly_RemovesCorrectElements()
+    {
+        NodeList<int> list = [1, 2, 3, 4, 5];
+
+        list.RemoveAt(2);
+        list.RemoveAt(2);
+
+        int[] expected = [1, 2, 5];
+        int index = 0;
+
+        foreach (var item in list)
+        {
+            Assert.Equal(expected[index++], item);
+        }
+
+        Assert.Equal(3, list.Count);
+    }
+
+    [Fact]
+    public void RemoveAt_FromLargeList_RemovesCorrectElement()
+    {
+        NodeList<int> list = [];
+
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        list.RemoveAt(50);
+
+        Assert.Equal(99, list.Count);
+
+        int expected = 0;
+
+        foreach (var item in list)
+        {
+            if (expected == 50)
+            {
+                expected++;
+            }
+
+            Assert.Equal(expected++, item);
+        }
+    }
+}
